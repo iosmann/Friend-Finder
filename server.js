27b36c7b78df0f23bb249ff043a51b37a1086e6c
ileysosman <ileys.osmann@gmail.com>
2018-06-13 20:18:20 -0500

    port change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 // EXPRESS CONFIGURATION
 var app = express();
 
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
